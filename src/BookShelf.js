caf4b211d354bf33b9d7beb24994997b6ef8b1ec
DennisM83 <dennisfinley@gmail.com>
2018-09-19 21:42:08 -0400

    added array of titles for the shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import Books from './Books'
+
 class BookShelf extends Component {
     render () {
+        let title = [
+            {name: 'Currently Reading '}, 
+            {name: 'Want To Read '}, 
+            {name:'Read'}
+        ]
+        let shelfTitle = title.filter(title => title.name.length > 15).map(title => title.name, title.index)
         console.log(this.props.bookList)
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Want To Read</h2>
+                <h2 className="bookshelf-title">{shelfTitle}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {this.props.bookList.map(book => 
+                        {/*written by Maeva NAP https://www.youtube.com/watch?v=i6L2jLHV9j8 */}
+                        {this.props.bookList.filter(book => book.shelf === 'read').map(book => 
                         (<li key={book.id}>
-                            <Books />
+                            <Books
+                                name = {book.name}
+                            />
                         </li>))}
                     </ol>
                 </div>
