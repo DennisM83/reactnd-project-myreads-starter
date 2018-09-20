7239f6c241e37817888560d7b9b455b86ed8c717
DennisM83 <dennisfinley@gmail.com>
2018-09-20 12:13:56 -0400

    added images, titles, and authors to books

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,25 +3,43 @@ import Books from './Books'
 
 class BookShelf extends Component {
     render () {
-        let title = [
-            {name: 'Currently Reading '}, 
-            {name: 'Want To Read '}, 
-            {name:'Read'}
-        ]
-        let shelfTitle = title.filter(title => title.name.length > 15).map(title => title.name, title.index)
         console.log(this.props.bookList)
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
+                <h2 className="bookshelf-title">Currently Reading</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {/*written by Maeva NAP https://www.youtube.com/watch?v=i6L2jLHV9j8 */}
-                        {this.props.bookList.filter(book => book.shelf === 'read').map(book => 
+                        {this.props.bookList.filter(book => book.shelf === 'currentlyReading').map(book => 
                         (<li key={book.id}>
                             <Books
-                                name = {book.name}
+                                book={book}
                             />
                         </li>))}
+                    </ol>
+                </div>
+                <h2 className="bookshelf-title">Want To Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {/*written by Maeva NAP https://www.youtube.com/watch?v=i6L2jLHV9j8 */}
+                        {this.props.bookList.filter(book => book.shelf === 'wantToRead').map(book =>
+                            (<li key={book.id}>
+                                <Books
+                                    book={book}
+                                />
+                            </li>))}
+                    </ol>
+                </div>
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {/*written by Maeva NAP https://www.youtube.com/watch?v=i6L2jLHV9j8 */}
+                        {this.props.bookList.filter(book => book.shelf === 'read').map(book =>
+                            (<li key={book.id}>
+                                <Books
+                                    book={book}
+                                />
+                            </li>))}
                     </ol>
                 </div>
             </div>
